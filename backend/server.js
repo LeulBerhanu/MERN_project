21f202335f0adcdd02ee6b5304d9d796bfb2fb28
LeulBerhanu <leulberhanua@gmail.com>
2023-12-11 14:25:44 +0300

    setup express

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
+const port = process.env.PORT;
 
-app.listen(4000, () => {
-  console.log("listening on port 4000 ...");
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to the app" });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port} ...`);
+});
