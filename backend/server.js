7ad42be1b786fbde1767e18575620bcad537462f
LeulBerhanu <leulberhanua@gmail.com>
2023-12-11 20:06:43 +0300

    setup model and schema

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 const app = express();
@@ -16,7 +18,15 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRoutes);
 
-// listen for requests
-app.listen(port, () => {
-  console.log(`listening on port ${port} ...`);
-});
+// Connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(port, () => {
+      console.log(`connected to db & listening on port ${port} ...`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
