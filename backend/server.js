d9229e4e4794c2afac672aaa9efebd0a6de629d2
LeulBerhanu <leulberhanua@gmail.com>
2023-12-11 14:43:18 +0300

    setup workout route

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 const app = express();
 const port = process.env.PORT;
+
+// middleware
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.json({ message: "welcome to the app" });
-});
+// routes
+app.use("/api/workouts", workoutRoutes);
 
+// listen for requests
 app.listen(port, () => {
   console.log(`listening on port ${port} ...`);
 });
