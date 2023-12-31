99e1d159ad2d275ac356aaad40b6cd3d7a687c5a
LeulBerhanu <leulberhanua@gmail.com>
2023-12-31 11:43:05 +0300

    uesLogin

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password);
+    await login(email, password);
   };
 
   return (
@@ -27,15 +29,16 @@ const Login = () => {
 
       <label>Password:</label>
       <input
-        type="email"
+        type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         className="w-full mb-4 border-2 p-1"
       />
 
-      <button type="submit" className="btn-primary">
+      <button type="submit" className="btn-primary" disabled={isLoading}>
         Login
       </button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
