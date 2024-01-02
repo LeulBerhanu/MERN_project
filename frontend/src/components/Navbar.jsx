f1a30e97cb8f8da2c6e3389bafd0d2836ad2380f
LeulBerhanu <leulberhanua@gmail.com>
2024-01-02 21:38:09 +0300

    protect api routes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
+  console.log("USER:", user);
 
   const handleLogout = () => {
     logout();
@@ -19,18 +22,22 @@ function Navbar() {
           <h1 className="text-4xl">Workout Buddy</h1>
         </NavLink>
         <nav className="flex items-center">
-          <div>
-            <button
-              onClick={handleLogout}
-              className="text-primary px-1 py-2 rounded-[4px] border-2 border-primary"
-            >
-              Log out
-            </button>
-          </div>
-          <div className="flex items-center gap-3">
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/signup">Signup</NavLink>
-          </div>
+          {user ? (
+            <div>
+              <span>{user.email}</span>
+              <button
+                onClick={handleLogout}
+                className="text-primary px-1 py-2 rounded-[4px] border-2 border-primary"
+              >
+                Log out
+              </button>
+            </div>
+          ) : (
+            <div className="flex items-center gap-3">
+              <NavLink to="/login">Login</NavLink>
+              <NavLink to="/signup">Signup</NavLink>
+            </div>
+          )}
         </nav>
       </div>
     </header>
